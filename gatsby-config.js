911a81695c810814d88dd81abc63d29a3a2bab11
Iliyan Vangelov <iliyan@st6.io>
2022-07-29 11:43:50 +0300

    Repro steps for gaOutput bug
    - when plugin is used with multiple "trackingIds" and "gtagConfig.anonymize_ip: true", the gaOptout() fn doesn`t optOut for all provided trackindIds, but only for the 1st one and network request related to other trackingIds are being sent to google

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-gtag",
+      options: {
+        trackingIds: ["G-ID1", "G-ID2"],
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+      },
+    },
+  ],
+};
